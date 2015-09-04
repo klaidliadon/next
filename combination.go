d79c57d69706590c3e3046000c538ca2c9541986
Alex Guerrieri <alex@tyba.com>
2015-09-04 15:19:29 +0200

    Fixed random behaviour. Uint -> int

--- combination.go
+++ combination.go
@@ -5,7 +5,14 @@ import "sync"
 // An element of the combination
 type element struct {
 	prev         *element
-	depth, index uint
+	depth, index int
+}
+
+func (e *element) list() []int {
+	if e.prev == nil {
+		return []int{e.index}
+	}
+	return append(e.prev.list(), e.index)
 }
 
 // The combination struct
@@ -14,14 +21,15 @@ type Combination struct {
 }
 
 // Returns a channel of possible combinations
-func (c *Combination) Results(l uint) <-chan []interface{} {
-	wg, ch, bs := new(sync.WaitGroup), make(chan []interface{}), uint(len(c.Base))
+func (c *Combination) Results(l int) <-chan []interface{} {
+	wg, ch, bs := new(sync.WaitGroup), make(chan []interface{}), len(c.Base)
 	defer func() { go func() { wg.Wait(); close(ch) }() }()
 	if l < 1 || l > bs {
 		return ch
 	}
 	wg.Add(1)
 	if l == 1 {
+		// simple case, every element in the base
 		go func() {
 			defer wg.Done()
 			for _, v := range c.Base {
@@ -30,32 +38,35 @@ func (c *Combination) Results(l uint) <-chan []interface{} {
 		}()
 		return ch
 	} else if l == bs {
+		// simple case, 1 result (the base)
 		go func() { defer wg.Done(); ch <- c.Base }()
 		return ch
 	}
 	max := bs - l
-	wg.Add(int(max))
-	for i := uint(0); i < max+1; i++ {
-		go func(l, i uint) { defer wg.Done(); c.thread(wg, ch, l, i) }(l, i)
+	wg.Add(max)
+	for i := int(0); i < max+1; i++ {
+		go func(l, i int) { defer wg.Done(); c.thread(wg, ch, l, i) }(l, i)
 	}
 	return ch
 }
 
 // A single thread where the first element is fixed
-func (c *Combination) thread(wg *sync.WaitGroup, ch chan []interface{}, l, s uint) {
-	e, bs := &element{index: s}, uint(len(c.Base))
+func (c *Combination) thread(wg *sync.WaitGroup, ch chan []interface{}, l, s int) {
+	e, bs := &element{index: s}, len(c.Base)
 	for i, max := s, bs+2-l; e.depth != 1 || e.index < max; i++ {
+		// if the next element is out of range
 		if i+1 == bs {
+			// if there is no previous element stop
 			if e.prev == nil {
 				break
 			}
-			i = e.index - 1
-			e = e.prev
+			// or go to the previous element using the index
+			i, e = e.index-1, e.prev
 			continue
 		}
 		e = &element{index: i + 1, depth: e.depth + 1, prev: e}
 		if e.depth+1 == l {
-			ch <- value(c, e)
+			ch <- c.value(e)
 			i = e.index - 1
 			e = e.prev
 		}
@@ -63,12 +74,11 @@ func (c *Combination) thread(wg *sync.WaitGroup, ch chan []interface{}, l, s uin
 }
 
 // Returns the actual combination using an element
-func value(c *Combination, e *element) []interface{} {
-	result := make([]interface{}, e.depth+1)
-	var curr = e
-	for i := uint(0); i <= e.depth; i++ {
-		result[e.depth-i] = c.Base[curr.index]
-		curr = curr.prev
+func (c *Combination) value(e *element) []interface{} {
+	idxlist := e.list()
+	result := make([]interface{}, len(idxlist))
+	for i := 0; i < e.depth+1; i++ {
+		result[e.depth-i] = c.Base[idxlist[i]]
 	}
 	return result
 }
--- combination_test.go
+++ combination_test.go
@@ -23,17 +23,28 @@ func init() {
 }
 
 func TestCombinationSize(t *testing.T) {
-	c := Combination{Base: []interface{}{"a", "b", "c", "d", "e", "f", -1, 3, 76, 1000, []interface{}{}, 0, 898, 23, 239, 29, 0x22, "sed"}}
+	c := Combination{Base: []interface{}{
+		"0a", "0b", "0c", "0d",
+		"1a", "1b", "1c", "1d",
+		"2a", "2b", "2c", "2d",
+		"3a", "3b", "3c", "3d",
+		"4a", "4b", "4c", "4d",
+	}}
 	size := len(c.Base)
 	for i := 0; i < size+2; i++ {
-		var count int
-		for range c.Results(uint(i)) {
-			count++
-		}
+		count := countResults(c, i)
 		if expected := combinationSize(size, i); count != expected {
 			t.Errorf("(%2d %2d) = %d (expected %d)", size, i, count, expected)
 		} else {
 			t.Logf("(%2d %2d) = %d", size, i, count)
 		}
 	}
+}
+
+func countResults(c Combination, i int) int {
+	var count int
+	for range c.Results(i) {
+		count++
+	}
+	return count
 }
