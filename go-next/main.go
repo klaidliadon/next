40b2b7e2efeb6db2115acb8b2e6559e1a3b8285d
Alex Guerrieri <alex@tyba.com>
2015-11-06 18:22:16 +0100

    better exec

--- go-next/main.go
+++ go-next/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/klaidliadon/next"
 )
@@ -11,14 +12,22 @@ import (
 func main() {
 	var o bool
 	var r bool
-	var s uint = 3
+	var s uint
 	flag.BoolVar(&o, "order", false, "order matters")
 	flag.BoolVar(&r, "repeat", false, "repeatition")
-	flag.UintVar(&s, "size", 0, "size of each result")
+	flag.UintVar(&s, "size", 0, "size of each result (not 0)")
 	flag.Parse()
 	var args []interface{}
 	for _, v := range flag.Args() {
 		args = append(args, v)
+	}
+	if s == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if len(args) == 0 {
+		fmt.Printf("%s: please specify at least an element.\n", os.Args[0])
+		os.Exit(1)
 	}
 	var ch <-chan []interface{}
 	if o {
